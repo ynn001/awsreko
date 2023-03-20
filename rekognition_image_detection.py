fb11aea544f5ef56dee78fa1f2aee35819bd4f86
ynn001 <2690974711@qq.com>
2023-03-20 20:10:41 +0800

    ynn

--- rekognition_image_detection.py
+++ rekognition_image_detection.py
@@ -236,7 +236,7 @@ def usage_demo():
     three_girls_url = 'https://dhei5unw3vrsx.cloudfront.net/images/target3_resized.jpg'
     swimwear_object = boto3.resource('s3').Object(
         'console-sample-images-pdx', 'yoga_swimwear.jpg')
-    book_file_name = './media/backgroud.png'
+    book_file_name = './media/ynn.webp'
 
     street_scene_image = RekognitionImage.from_file(
         street_scene_file_name, rekognition_client)
